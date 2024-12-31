4012ef9ff737406311b4cf3342131cb35ab38c26
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2025-01-01 01:28:15 +0700

    feat : response headers to allow cors

--- src/app/api/mock/route.ts
+++ src/app/api/mock/route.ts
@@ -4,14 +4,19 @@ import { renderMocker } from '@/utils/mock'
 import { NextRequest, NextResponse } from 'next/server'
 
 async function handler(request: NextRequest) {
+
+    const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, PATCH, OPTIONS'); // Allow specific methods
+
     const query = request.nextUrl.searchParams.get(SPECIAL_QUERY_PARAMS_KEY)
 
     try {
         const { field } = decodeFieldTree(query ?? '')
         const jsonData = renderMocker(field)
-        return NextResponse.json(jsonData)
+        return NextResponse.json(jsonData, { headers })
     } catch (err) {
-        return NextResponse.json({ error: err }, { status: 400 })
+        return NextResponse.json({ error: err }, { status: 400, headers })
     }
 }
 
